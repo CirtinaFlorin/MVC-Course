605761ecbf3c0faba94cae61f5e3d5e117a6d0a1
CirtinaFlorin <florin.cirtina@outlook.com>
2016-11-21 21:48:05 +0200

    MVC-Course-7

---
+++ MVC-Course7/MVC-Course7/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Course7.Controllers
+{
+    //Exercise 1 - Course 7
+    public class HomeController: Controller
+    {
+        [ActionName ("about")]
+        public ActionResult SomeMethodName() {
+            return View();
+        }
+        public ActionResult index()
+        {
+            return View();
+        }
+
+        //Exercise 3 - Course 7
+        [NonAction]
+        public int Calc() {
+            return 1 + 2 + 3;
+        }
+
+        public int displayCalc() {
+            return Calc();
+        }
+
+        //Exercise 4 - Course 7
+        [HttpGet]
+        public ActionResult VerbTest() {
+            return Content ("Hello");
+        }
+
+        //Exercise 5 - Course 7
+        [ActionName ("authorize")]
+        [Authorize (Roles = "Admin")]
+        public ActionResult ThisMethodNeedsAuthorization() {
+            return Content("Hello");
+        }
+
+
+    }
+}
---
+++ MVC-Course7/MVC-Course7/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC_Course7.MvcApplication" Language="C#" %>
